d3877558f5bf3095052fe4dd994323cbe258d2a7
Fri5Day <victoramaral951@outlook.com>
2023-09-08 22:00:30 -0300

    build: primeiro build do projeto

---
+++ dist/assets/VoltaPaginaAnterior-41d8cd56.js
@@ -0,0 +1,1 @@
+import{_ as o,o as t,C as a,w as r,f as e,D as s,B as n}from"./index-37c69531.js";const i={methods:{click(){this.$router.back()}}};function l(_,f,m,p,d,c){return t(),a(n,{class:"mt-2 ml-2",color:"primary",onClick:c.click},{default:r(()=>[e(s,{icon:"mdi-arrow-left"})]),_:1},8,["onClick"])}const u=o(i,[["render",l]]);export{u as V};
